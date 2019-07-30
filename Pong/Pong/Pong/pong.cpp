a72eb016a6fdab4b5fce3de56344d70de1645369
Ryan Robinson <ryantherileyman@gmail.com>
2019-07-30 18:30:35 -0500

    Added ball, collision detection, and scoring to Pong game

--- Pong/Pong/Pong/pong.cpp
+++ Pong/Pong/Pong/pong.cpp
@@ -19,7 +19,7 @@ int interval = 1000 / 60;
 int score_left = 0;
 int score_right = 0;
 
-// Rackets shape
+// Racket properties
 int racket_width = 10;
 int racket_height = 80;
 int racket_speed = 3;
@@ -31,6 +31,17 @@ float racket_left_y = 50.0f;
 // Right racket position
 float racket_right_x = width - racket_width - 20;
 float racket_right_y = 50.0f;
+
+// Ball properties
+int ball_size = 8;
+int ball_speed = 2;
+
+// Ball position
+float ball_pos_x = width / 2;
+float ball_pos_y = height / 2;
+float ball_dir_x = -1.0f;
+float ball_dir_y = 0.0f;
+
 
 std::string int2str(int x) {
 	std::stringstream result;
@@ -72,6 +83,9 @@ void draw() {
 	drawRect(racket_left_x, racket_left_y, racket_width, racket_height);
 	drawRect(racket_right_x, racket_right_y, racket_width, racket_height);
 
+	// Draw ball
+	drawRect(ball_pos_x - ball_size / 2, ball_pos_y - ball_size / 2, ball_size, ball_size);
+
 	// Draw score
 	drawText(width / 2 - 10, height - 20, int2str(score_left) + ":" + int2str(score_right));
 
@@ -97,9 +111,82 @@ void pollKeyboardInputs() {
 	}
 }
 
+void vec2_norm(float& x, float& y) {
+	float length = sqrt((x * x) + (y * y));
+	if (length != 0.0f) {
+		length = 1.0f / length;
+		x *= length;
+		y *= length;
+	}
+}
+
+void updateBall() {
+	// Fly in current direction
+	ball_pos_x += ball_dir_x * ball_speed;
+	ball_pos_y += ball_dir_y * ball_speed;
+
+	// Check for collision with left racket
+	if (
+		(ball_pos_x < racket_left_x + racket_width) &&
+		(ball_pos_x > racket_left_x) &&
+		(ball_pos_y < racket_left_y + racket_height) &&
+		(ball_pos_y > racket_left_y)
+	) {
+		float t = ((ball_pos_y - racket_left_y) / racket_height) - 0.5f;
+		ball_dir_x = fabs(ball_dir_x);
+		ball_dir_y = t;
+	}
+
+	// Check for collision with right racket
+	if (
+		(ball_pos_x > racket_right_x) &&
+		(ball_pos_x < racket_right_x + racket_width) &&
+		(ball_pos_y < racket_right_y + racket_height) &&
+		(ball_pos_y > racket_right_y)
+	) {
+		float t = ((ball_pos_y - racket_right_y) / racket_height) - 0.5f;
+		ball_dir_x = -fabs(ball_dir_x);
+		ball_dir_y = t;
+	}
+
+	// Check for collision with left wall
+	if (ball_pos_x < 0) {
+		score_right++;
+		ball_pos_x = width / 2;
+		ball_pos_y = height / 2;
+		ball_dir_x = fabs(ball_dir_x);
+		ball_dir_y = 0;
+	}
+
+	// Check for collision with right wall
+	if (ball_pos_x > width) {
+		score_left++;
+		ball_pos_x = width / 2;
+		ball_pos_y = height / 2;
+		ball_dir_x = -fabs(ball_dir_x);
+		ball_dir_y = 0;
+	}
+
+	// Check for collision with top wall
+	if (ball_pos_y > height) {
+		ball_dir_y = -fabs(ball_dir_y);
+	}
+
+	// Check for collision with bottom wall
+	if (ball_pos_y < 0) {
+		ball_dir_y = fabs(ball_dir_y);
+	}
+
+	// Make sure the length of the ball direction stays at 1
+	vec2_norm(ball_dir_x, ball_dir_y);
+}
+
 void update(int value) {
 	// Handle inputs
 	pollKeyboardInputs();
+
+	// Update ball position
+	updateBall();
 
 	// Call 'update' again in interval seconds
 	glutTimerFunc(interval, update, 0);
